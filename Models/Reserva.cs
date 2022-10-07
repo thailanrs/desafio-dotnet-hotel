8ab953f69cceda71be29db930ce14b50ab161ff6
Thailan Ramos <thailanrs@gmail.com>
2022-10-06 20:46:39 -0400

    desafio de código DIO - Cadastro de reserva de Suítes em Hotel

---
+++ Models/Reserva.cs
@@ -0,0 +1,55 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // Verifica se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // Retorna uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                throw new ArgumentException("O número de hóspedes é maior que a capacidade suportada.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // Retorna a quantidade de hóspedes (propriedade Hospedes)
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // Retorna o valor da diária
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            // Caso os dias reservados forem maior ou igual a 10, concede um desconto de 10%
+            if (DiasReservados >= 10)
+            {
+                valor = valor * 0.9M ;
+            }
+
+            return valor;
+        }
+    }
+}
